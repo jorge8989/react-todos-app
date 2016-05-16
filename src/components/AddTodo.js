de33e798eda4f9fbf478a270063ab93d2ef5ca29
jorge <jorgearteaga8936@gmail.com>
2016-05-16 19:31:19 -0400

    added form for adding tasks

---
+++ src/components/AddTodo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from '../Actions'
+
+let AddTodo = ({ dispatch }) => {
+  let input
+
+  return (
+    <form class="form-inline" onSubmit={(event) => {
+      event.preventDefault();
+      dispatch(addTodo(input.value));
+      input.value = '';
+    }}>
+     <div class="form-group">
+        <input ref={node => {
+          input = node
+        }} class="form-control"/>
+        <button class="btn btn-primary">
+          Add Todo
+        </button>
+      </div>
+    </form>
+  )
+}
+AddTodo = connect()(AddTodo)
+
+export default AddTodo
+export default AddTodo;
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import AddTodo from './AddTodo';
 
 const App = () => (
   <div class="container">
     <h1>Todo List</h1>
+    <AddTodo/>
     <VisibleTodoList />
   </div>
 )
